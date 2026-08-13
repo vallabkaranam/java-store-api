654f4778e031d70a96dd3e89aac8774c19fc099c
vallabkaranam <15883626+vallabkaranam@users.noreply.github.com>
2026-08-12 17:24:06 -0700

    Replace static index.html with Thymeleaf SSR template for dynamic views, input from model

--- src/main/java/com/codewithmosh/store/controllers/HomeController.java
+++ src/main/java/com/codewithmosh/store/controllers/HomeController.java
@@ -3,15 +3,16 @@ package com.codewithmosh.store.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.ui.Model;
+
 @Controller
 public class HomeController {
+
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("name", "Vallab");
+
+        return "index";
     }
     
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "index.html";
-    }
 }
