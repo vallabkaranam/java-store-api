2ed618e3a85701b4345bdbd48ad110b936729837
vallabkaranam <15883626+vallabkaranam@users.noreply.github.com>
2026-08-19 15:38:54 -0700

    Permit internal error dispatches

--- src/main/java/com/vallab/store/common/ErrorSecurityRules.java
+++ src/main/java/com/vallab/store/common/ErrorSecurityRules.java
@@ -1,5 +1,6 @@
 package com.vallab.store.common;
 
+import jakarta.servlet.DispatcherType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,6 @@ import org.springframework.stereotype.Component;
 public class ErrorSecurityRules implements SecurityRules {
     @Override
     public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
-        registry.requestMatchers("/error").permitAll();
+        registry.dispatcherTypeMatchers(DispatcherType.ERROR).permitAll();
     }
 }
